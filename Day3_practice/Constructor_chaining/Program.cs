870a732d77e2a360d85525d4eeb828f108e26565
Amal Assem <amalassem21@gmail.com>
2021-05-26 03:11:25 +0200

    Day3 Practice

---
+++ Day3_practice/Constructor_chaining/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Constructor_chaining
+{
+	//class student
+	class Student
+	{
+		#region variables
+		public static int count;
+		#endregion
+
+		#region Properities
+		public int id{ get; set; }
+		public string name { get; set; }
+		public int age { get; set; }
+		public string address { get; set; }
+		#endregion
+
+		#region constructor
+		static Student()
+		{
+			count=0;
+		}
+		public Student()
+		{
+
+		}
+		public Student(int id,string name)
+		{
+			count++;
+			this.id = id;
+			this.name = name;
+		}
+		public Student(int id, string name,int age):this(id,name)
+		{
+			this.age = age;
+		}
+		public Student(int id, string name, int age,string address) : this(id, name,age)
+		{
+			this.address = address;
+		}
+
+		#endregion
+		#region Methods
+		public override string ToString()
+		{
+			return $"ID : {id},\t  Name : {name},\t Age : {age},\t Address : {address}";
+		}
+		public static bool operator == (Student s1,Student s2)
+		{
+			return (s1.id==s2.id && s1.name==s2.name && s1.age==s2.age&&s1.address==s2.address);
+		}
+		public static bool operator !=(Student s1, Student s2)
+		{
+			return (s1.id == s2.id || s1.name == s2.name || s1.age == s2.age || s1.address == s2.address);
+		}
+		public static Student operator + (Student s1, Student s2)
+		{
+			Student s = new Student();
+			s.id = s1.id + s2.id;
+			s.name = s1.name +" "+ s2.name;
+			s.age = s1.age + s2.age;
+			s.address = s1.address;
+			return s;
+		}
+		#endregion
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Student s1 = new Student(1, "Amal",10,"Cairo");
+			Student s2 = new Student(2, "Assem",15,"Alex");
+
+			Console.WriteLine("\n*****************Data*****************");
+			Console.WriteLine($"Student1 : {s1}");
+			Console.WriteLine($"Student2 : {s2}");
+
+			Console.WriteLine("\n***************** == *****************");
+			if (s1 == s2)
+			{
+				Console.WriteLine("The data of two students are the same");
+			}
+			else
+			{
+				Console.WriteLine("\nThe data of two students are different");
+
+			}
+
+			Console.WriteLine("\n***************** != *****************");
+			if (s1 != s2)
+			{
+				Console.WriteLine("The data of two students are different");
+			}
+			else
+			{
+				Console.WriteLine("The data of two students are the same");
+
+			}
+
+			Console.WriteLine("\n***************** + *****************");
+			Console.WriteLine(s1+s2);
+
+			Console.WriteLine("\n***************** The number of students *****************");
+			Console.WriteLine($"# students : {Student.count}");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Day3_practice/PassByValue_PassByReference/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace PassByValue_PassByReference
+{
+	class student
+	{
+		#region Properities
+		public int id { get; set; }
+		public string name { get; set; }
+		public int age { get; set; }
+		public string address { get; set; }
+		#endregion
+
+		#region constructor
+
+		public student(int id, string name)
+		{
+			this.id = id;
+			this.name = name;
+		}
+		#endregion
+		#region Methods
+		public override string ToString()
+		{
+			return $"ID : {id},    Name : {name}";
+		}
+		#endregion
+	}
+	class Operation
+	    {
+		#region Pass by value
+		//pass value type by value
+		public static void Swap(int num1, int num2)
+		{
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+		//pass Reference type by value
+		public static void Swap(student s1, student s2)
+		{
+			student s = s1;
+			s1 = s2;
+			s2 = s;
+		}
+
+		#endregion
+		#region Pass by value
+		//pass value type by Reference
+		public static void Swap(ref int num1, ref int num2)
+		{
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+		//pass Reference type by Reference
+		public static void Swap(ref student s1, ref student s2)
+		{
+			student s = s1;
+			s1 = s2;
+			s2 = s;
+		}
+
+		#endregion
+
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Value type
+			int n1 = 5, n2 = 9;
+			//Reference type
+			student s1 = new student(1, "Amal");
+			student s2 = new student(2, "Assem");
+			
+			//pass value type by value
+			Console.WriteLine("Before Swapping value type by value:");
+			Console.WriteLine($"Number1 = {n1}\tNumber2 = {n2}");
+			Operation.Swap(n1, n2);
+			Console.WriteLine("\nAfter Swapping value type by value:");
+			Console.WriteLine($"Number1 = {n1}\tNumber2 = {n2}");
+
+			//pass value type by reference
+			Console.WriteLine("\nBefore Swapping value type by reference:");
+			Console.WriteLine($"Number1 = {n1}\tNumber2 = {n2}");
+			Operation.Swap(ref n1, ref n2);
+			Console.WriteLine("\nAfter Swapping value type by reference:");
+			Console.WriteLine($"Number1 = {n1}\tNumber2 = {n2}");
+
+			//pass reference type by value
+			Console.WriteLine("\nBefore Swapping value type by value:");
+			Console.WriteLine($"Student1 : {s1}\nStudent2 : {s2}");
+			Operation.Swap(s1, s2);
+			Console.WriteLine("\nAfter Swapping value type by value:");
+			Console.WriteLine($"Student2 : {s1}\nStudent2 : {s2}");
+
+			//pass reference type by reference
+			Console.WriteLine("\nBefore Swapping value type by reference:");
+			Console.WriteLine($"Student1 : {s1}\nStudent2 : {s2}");
+			Operation.Swap(ref s1, ref s2);
+			Console.WriteLine("\nAfter Swapping value type by reference:");
+			Console.WriteLine($"Student2 : {s1}\nStudent2 : {s2}");
+
+			Console.ReadKey();
+		}
+	}
+}
